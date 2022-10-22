9f69b85336f1a9a674d78fdabe94ac82930c6d30
tsunpoko <exploit.py@gmail.com>
2022-10-23 00:33:04 +0900

    8. リストと key

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -279,6 +279,50 @@ class Page extends React.Component {
   }
 }
 
+const numbers = [1, 2, 3, 4, 5]
+
+function NumberList(props) {
+  const numbers = props.numbers
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>
+      {number}
+    </li>
+  )
+  return (
+    <ul>{listItems}</ul>
+  )
+}
+
+const posts = [
+  {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+  {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+];
+
+function Blog(props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  )
+  const content = props.posts.map((post) =>
+		<div key={post.id}>
+			<h3>{post.title}</h3>
+			<p>{post.content}</p>
+		</div>
+  )
+  return (
+    <div>
+      {sidebar}
+			<hr />
+			{content}
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
@@ -296,6 +340,8 @@ function App() {
         <Page />
         <Form />
         <Toggle />
+        <NumberList numbers={numbers} />
+				<Blog posts={posts} />
         {element}
       </header>
     </div>
