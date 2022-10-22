587bde42399570b31867e729c42eab2f597a6f6c
tsunpoko <exploit.py@gmail.com>
2022-10-22 20:16:17 +0900

    7. 条件付きレンダーまで

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 
 function getGreeting(user) {
@@ -21,7 +21,6 @@ const user = {
   avatarUrl: 'https://cdn.pixabay.com/photo/2022/10/04/07/23/alien-7497513_1280.png'
 }
 
-// const element = <img src={user.avatarUrl} />
 const element = (
   <div>
     {getGreeting(user)}
@@ -29,11 +28,274 @@ const element = (
   </div>
 )
 
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
+
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  )
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user}></Avatar>
+      <div className="UserInfo-name">
+          {props.user.name}
+      </div>
+    </div>
+  )
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={props.author}></UserInfo>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+// function Clock(props) {
+//   return (
+//     <div>
+//       <h2>It is {props.date.toLocaleTimeString()}.</h2>
+//     </div>
+//   )
+// }
+
+function FormattedDate(props) {
+  return <h2>It is {props.date.toLocaleTimeString()}.</h2>
+}
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {date: new Date()}
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID)
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <FormattedDate date={this.state.date} />
+      </div>
+    )
+  }
+}
+
+function Form() {
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log('You clicked submit.')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {isToggleOn: true};
+    // This binding is necessary to make `this` work in the callback
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
+    }))
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    )
+  }
+}
+
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn
+  if (isLoggedIn) {
+    return <UserGreeting />
+  } else {
+    return <GuestGreeting />
+  }
+}
+
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  );
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  );
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleLoginClick = this.handleLoginClick.bind(this)
+    this.handleLogoutClick = this.handleLogoutClick.bind(this)
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn
+    let button
+    if (isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />
+    }
+    return (
+      <div>
+        <Greeting isLoggedIn={false} />
+        {button}
+      </div>
+    )
+  }
+
+}
+
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 &&
+        <h2>
+          You have {unreadMessages.length} unread messages.
+        </h2>
+      }
+    </div>
+  )
+}
+
+const messages = ['React', 'Re: React', 'Re:Re: React'];
+
+function WarningBanner(props) {
+  if (!props.warn) {
+    return null
+  }
+
+  return (
+    <div className="warning">
+      Warning!
+    </div>
+  )
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {showWarning: true}
+    this.handleToggleClick = this.handleToggleClick.bind(this)
+  }
+
+  handleToggleClick() {
+    this.setState(state => ({
+      showWarning: !state.showWarning
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? 'Hide' : 'Show'}
+        </button>
+      </div>
+    )
+  }
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Welcome name="Sara"></Welcome>
+        <Clock />
+        <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}
+        />
+        <LoginControl />
+        <Mailbox unreadMessages={messages} />
+        <Page />
+        <Form />
+        <Toggle />
         {element}
       </header>
     </div>
