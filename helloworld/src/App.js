c610ef0fc7366acc200a6bf7331fe8b681a96576
tsunpoko <exploit.py@gmail.com>
2022-10-23 19:40:31 +0900

    11. コンポジション vs 継承

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -568,6 +568,48 @@ class TemperatureInput extends React.Component {
 	}
 }
 
+function FancyBorder(props) {
+	return (
+		<div className={'FancyBorder FancyBorder-' + props.color}>
+			{props.children}
+		</div>
+	)
+}
+
+function WelcomeDialog() {
+	return (
+		<FancyBorder color="blue">
+			<h1 className="Dialog-title">
+				Welcome
+			</h1>
+			<p className="Dialog-message">
+        Thank you for visiting our spacecraft!
+      </p>
+		</FancyBorder>
+	)
+}
+
+function Contacts() {
+  return <div className="Contacts" />;
+}
+
+function Chat() {
+  return <div className="Chat" />;
+}
+
+function SplitPane(props) {
+	return (
+		<div className="SplitPane">
+			<div className="SplitPane-left">
+				{props.left}
+			</div>
+			<div className="SplitPane-right">
+				{props.right}
+			</div>
+		</div>
+	)
+}
+
 function App() {
   return (
     <div className="App">
@@ -592,6 +634,14 @@ function App() {
 				<FlavorForm />
 				<Reservation />
 				<Calculator />
+				<WelcomeDialog />
+				<SplitPane
+					left={
+						<Contacts />
+					}
+					right={
+						<Chat />
+					} />
         {element}
       </header>
     </div>
